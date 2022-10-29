fdc56155caac0454227ae740ccb5444239edf862
Lucas Akio Ogawa <ogawalucas@gmail.com>
2022-10-29 15:17:07 -0300

    Adding Activity 08

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, FooterComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  constructor() {}
+  urlLogo: string;
+
+  constructor() {
+    this.urlLogo = '../assets/logo.png';
+  }
 
   ngOnInit(): void {}
 }
